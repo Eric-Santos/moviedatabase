7b770cd448a0b57c4bc100a1d3619da2dac79237
Eric <eric355402@yahoo.com>
2018-07-16 18:15:32 -0500

    input props

--- src/App.js
+++ src/App.js
@@ -4,32 +4,24 @@ import './App.css';
 
 
 class App extends Component {
-
-state = {
-  toggle: true
+submit = () => {
+  console.log(this.text.value);
+  console.log(this.email.value)
 }
-
-toggle = () => {
-  this.setState({
-    toggle:!this.state.toggle
-  })
-}
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text= 'Welcome to React' toggle={this.state.toggle}/>
+          
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        {this.state.toggle && 
-        <p>This should show and hide</p>
-        }
+       <input type="text" ref={(input) => this.text = input} />
         
-        <button onClick={this.toggle}>Show/Hide</button>
+        <button onClick={this.submit}>Show Value</button>
+        
       </div>
     );
   }
@@ -37,8 +29,8 @@ toggle = () => {
 
 class Welcome extends Component{
   render() {
-    const {text, toggle} = this.props;
-    console.log(toggle)
+    const {text} = this.props;
+    
     return (
       <h1 className="App-title">{text}</h1>
     )
